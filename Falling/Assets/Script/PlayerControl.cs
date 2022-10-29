94ca780cad36d7eacdf71854a1c71114b29ba907
MNHyper <>
2022-10-29 13:43:13 +0300

    Jump

--- Falling/Assets/Script/PlayerControl.cs
+++ Falling/Assets/Script/PlayerControl.cs
@@ -5,13 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    [Header("Move Variables")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform wallCheckPos;
     [SerializeField] private LayerMask wallLayer;
 
+    [Header("Jump Variables")]
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float fullingSpeed;
+    [SerializeField] private float jumpTimer;
+
+
     private bool mustPatrol;
     private bool mustTurn;
+    private bool grounded;
+    private bool jumping;
+    private bool jumpPoint = false;
 
 
     // Start is called before the first frame update
@@ -27,13 +40,43 @@ public class PlayerControl : MonoBehaviour
         {
             Patrol();
         }
+
+        JumpDown();
     }
 
     private void FixedUpdate()
     {
+        IsGrounded();
+
         if (mustPatrol)
         {
             mustTurn = Physics2D.OverlapCircle(wallCheckPos.position, 0.1f, wallLayer);
+        }
+    }
+
+    private void IsGrounded()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void OnJump(InputValue value)
+    {
+        jumping = value.isPressed;
+
+        if (grounded && jumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            jumpPoint = false;
+            StartCoroutine(StopJump());
+        }
+    }
+
+    private void JumpDown()
+    {
+        if (grounded && jumpPoint == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+            jumpPoint = false;
         }
     }
 
@@ -62,6 +105,22 @@ public class PlayerControl : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(wallCheckPos.position, 0.1f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, 0.2f);
+    }
+
+    private IEnumerator StopJump()
+    {
+        yield return new WaitForSeconds(jumpTimer);
+        jumping = false;
+
+        if (!grounded && !jumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, fullingSpeed);
+            jumpPoint = true;
+        }
+
+
     }
 
 }
