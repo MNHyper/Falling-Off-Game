91e0f7c87a314b2747a67c0bdd85457009ba3944
MNHyper <>
2022-11-15 21:29:00 +0200

    try

---
+++ Falling/Assets/JumpSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSpawner : MonoBehaviour
+{
+    [SerializeField] private PlayerControl player;
+    private float TimeToSpawn;
+    float timer;
+    public List<GameObject> ObjectPrefabs = new List<GameObject>();
+    void Update()
+    {
+        TimeToSpawn = Random.Range(0.5f, 2f);
+
+        if (Time.time > timer)
+        {
+            timer = Time.time + TimeToSpawn;
+
+            int random = Random.Range(0, ObjectPrefabs.Count);
+
+            var block = Instantiate(ObjectPrefabs[random], transform.position, transform.rotation);
+            block.GetComponent<JumpEnemy>().player = player;
+        }
+    }
+}
+
